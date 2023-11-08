58b21ef2d8276e12e83d67f878bc2ec4785becdb
laraschke <l.raschke@chainsulting.de>
2023-11-08 12:41:39 +0100

    Add: NFT contract with EIP 4973

--- contracts/SoulboundNft.sol
+++ contracts/SoulboundNft.sol
@@ -1,8 +1,69 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {ERC721, ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721, IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {IERC4973} from "./IERC4973.sol";
 
-contract SoulboundNft is ERC721URIStorage {
-    constructor() ERC721("SoulboundNft", "SB-NFT") {}
+contract SoulboundNft is ERC721URIStorage, Ownable2Step, IERC4973 {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("SoulboundNft", "SB-NFT") Ownable(msg.sender) {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        _safeMint(to, _tokenIdCounter);
+        _setTokenURI(_tokenIdCounter, uri);
+        _tokenIdCounter++;
+    }
+
+    function unequip(uint256 tokenId) external {
+        require(ownerOf(tokenId) == msg.sender, "SB-Nft: not owner");
+        _burn(tokenId);
+    }
+
+    function give(
+        address to,
+        bytes calldata metadata,
+        bytes calldata signature
+    ) external returns (uint256) {
+        return 0;
+    }
+
+    function take(
+        address from,
+        bytes calldata metadata,
+        bytes calldata signature
+    ) external returns (uint256) {
+        return 0;
+    }
+
+    function decodeURI(
+        bytes calldata metadata
+    ) external returns (string memory) {
+        return "";
+    }
+
+    function balanceOf(
+        address owner
+    ) public view override(ERC721, IERC4973, IERC721) returns (uint256) {
+        return super.balanceOf(owner);
+    }
+
+    function ownerOf(
+        uint256 tokenId
+    ) public view override(ERC721, IERC4973, IERC721) returns (address) {
+        return super.ownerOf(tokenId);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721URIStorage) returns (bool) {
+        return
+            interfaceId == type(IERC4973).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,56 +1,45 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv'
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
-import "hardhat-contract-sizer";
-import "@primitivefi/hardhat-dodoc";
+import { HardhatUserConfig } from 'hardhat/config'
+import '@typechain/hardhat'
+import 'hardhat-gas-reporter'
+import 'solidity-coverage'
+import 'hardhat-contract-sizer'
 
-dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+dotenv.config()
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.7",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
+    solidity: {
+        version: '0.8.7',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-  },
-  networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_URL || '',
+            accounts:
+                process.env.PRIVATE_KEY !== undefined
+                    ? [process.env.PRIVATE_KEY]
+                    : [],
+        },
     },
-  },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-  contractSizer: {
-    runOnCompile: true,
-  },
-};
+    gasReporter: {
+        enabled: process.env.REPORT_GAS !== undefined,
+        currency: 'USD',
+    },
+    // etherscan: {
+    //     apiKey: process.env.ETHERSCAN_API_KEY,
+    // },
+    contractSizer: {
+        runOnCompile: true,
+    },
+}
 
-export default config;
+export default config
