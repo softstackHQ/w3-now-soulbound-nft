812736462040ab6bdfaed29a778b38932e79e394
laraschke <l.raschke@chainsulting.de>
2023-11-10 11:47:28 +0100

    Add: Same Metadata for every token - remove ERC721URIStorage

--- contracts/SoulboundNft.sol
+++ contracts/SoulboundNft.sol
@@ -2,34 +2,35 @@
 pragma solidity 0.8.21;
 
 import {ERC721, IERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract SoulboundNft is ERC721URIStorage, Ownable2Step {
+contract SoulboundNft is ERC721, Ownable2Step {
     uint256 private _tokenIdCounter;
+    string private _tokenURI;
 
     constructor(
         string memory name_,
         string memory symbol_,
+        string memory tokenURI_,
         address owner_
     ) ERC721(name_, symbol_) Ownable() {
         _transferOwnership(owner_);
+        _tokenURI = tokenURI_;
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to) public onlyOwner {
         require(balanceOf(to) == 0, "SB-Nft: already minted");
-        require(
-            bytes(_baseURI()).length != 0 || bytes(uri).length != 0,
-            "SB-Nft: no uri"
-        );
         _safeMint(to, _tokenIdCounter);
-        _setTokenURI(_tokenIdCounter, uri);
         _tokenIdCounter++;
     }
 
     function unequip(uint256 tokenId) external {
         require(ownerOf(tokenId) == msg.sender, "SB-Nft: not owner");
         _burn(tokenId);
+    }
+
+    function tokenURI(uint256) public view override returns (string memory) {
+        return _tokenURI;
     }
 
     function _beforeTokenTransfer(
--- test/soulboundNft.test.ts
+++ test/soulboundNft.test.ts
@@ -6,6 +6,7 @@ import { ethers } from "hardhat"
 describe("Soulbound Nft", function () {
     let soulboundNft: SoulboundNft
     let receiver: string
+    const tokenURI = "ipfs://testtokenuri"
     beforeEach(async function () {
         const signers = await ethers.getSigners()
         receiver = await signers[1].getAddress()
@@ -15,39 +16,38 @@ describe("Soulbound Nft", function () {
         soulboundNft = (await SoulboundNft.deploy(
             "SoulboundNft",
             "SB",
+            tokenURI,
             owner
         )) as unknown as SoulboundNft
     })
 
     it("Should mint NFT by owner", async function () {
-        await soulboundNft.safeMint(receiver, "ipfs://testtokenuri")
+        await soulboundNft.safeMint(receiver)
         expect(await soulboundNft.balanceOf(receiver)).to.equal(1)
         expect(await soulboundNft.ownerOf(0)).to.equal(receiver)
     })
 
     it("Should emit transfer event on minting", async function () {
-        await expect(soulboundNft.safeMint(receiver, "ipfs://testtokenuri"))
+        await expect(soulboundNft.safeMint(receiver))
             .to.emit(soulboundNft, "Transfer")
             .withArgs(ethers.ZeroAddress, receiver, 0)
     })
 
     it("Should set token URI properly", async function () {
-        const tokenUri1 = "ipfs://testtokenuri1"
-        await soulboundNft.safeMint(receiver, tokenUri1)
-        expect(await soulboundNft.tokenURI(0)).to.equal(tokenUri1)
+        await soulboundNft.safeMint(receiver)
+        expect(await soulboundNft.tokenURI(0)).to.equal(tokenURI)
 
         const [, , receiver2] = await ethers.getSigners()
         const receiverAddress2 = await receiver2.getAddress()
-        const tokenUri2 = "ipfs://testtokenuri2"
-        await soulboundNft.safeMint(receiverAddress2, tokenUri2)
-        expect(await soulboundNft.tokenURI(1)).to.equal(tokenUri2)
+        await soulboundNft.safeMint(receiverAddress2)
+        expect(await soulboundNft.tokenURI(1)).to.equal(tokenURI)
     })
 
     it("Should increment token id properly", async function () {
         const [, , receiver2] = await ethers.getSigners()
         const receiverAddress2 = await receiver2.getAddress()
-        await soulboundNft.safeMint(receiver, "testuri1")
-        await soulboundNft.safeMint(receiver2, "testuri2")
+        await soulboundNft.safeMint(receiver)
+        await soulboundNft.safeMint(receiver2)
         expect(await soulboundNft.balanceOf(receiver)).to.equal(1)
         expect(await soulboundNft.balanceOf(receiver2)).to.equal(1)
         expect(await soulboundNft.ownerOf(0)).to.equal(receiver)
@@ -56,48 +56,38 @@ describe("Soulbound Nft", function () {
 
     it("Should revert minting NFT to non receiving address", async function () {
         await expect(
-            soulboundNft.safeMint(ethers.ZeroAddress, "ipfs://testtokenuri")
+            soulboundNft.safeMint(ethers.ZeroAddress)
         ).to.be.revertedWith("ERC721: address zero is not a valid owner")
 
         const contractAddress = await soulboundNft.getAddress()
-        await expect(
-            soulboundNft.safeMint(contractAddress, "ipfs://testtokenuri")
-        ).to.be.revertedWith(
+        await expect(soulboundNft.safeMint(contractAddress)).to.be.revertedWith(
             "ERC721: transfer to non ERC721Receiver implementer"
         )
     })
 
     it("Should revert minting NFT to same address twice", async function () {
-        await soulboundNft.safeMint(receiver, "testtokenuri1")
+        await soulboundNft.safeMint(receiver)
         expect(await soulboundNft.balanceOf(receiver)).to.equal(1)
-        await expect(
-            soulboundNft.safeMint(receiver, "ipfs://testtokenuri2")
-        ).to.be.revertedWith("SB-Nft: already minted")
-    })
-
-    it("Should revert minting NFT with no uri provided", async function () {
-        await expect(soulboundNft.safeMint(receiver, "")).to.be.revertedWith(
-            "SB-Nft: no uri"
+        await expect(soulboundNft.safeMint(receiver)).to.be.revertedWith(
+            "SB-Nft: already minted"
         )
     })
 
     it("Should revert minting NFT by non-owner", async function () {
         const [, , nonOwner] = await ethers.getSigners()
         await expect(
-            soulboundNft
-                .connect(nonOwner)
-                .safeMint(receiver, "ipfs://testtokenuri")
+            soulboundNft.connect(nonOwner).safeMint(receiver)
         ).to.be.revertedWith("Ownable: caller is not the owner")
     })
 
     it("Should burn NFT by token owner", async function () {
         const [, receiverSigner] = await ethers.getSigners()
-        await soulboundNft.safeMint(receiver, "ipfs://testtokenuri")
+        await soulboundNft.safeMint(receiver)
         await soulboundNft.connect(receiverSigner).unequip(0)
     })
 
     it("Should revert buring NFT by non token owner", async function () {
-        await soulboundNft.safeMint(receiver, "ipfs://testtokenuri")
+        await soulboundNft.safeMint(receiver)
         await expect(soulboundNft.unequip(0)).to.be.revertedWith(
             "SB-Nft: not owner"
         )
@@ -106,7 +96,7 @@ describe("Soulbound Nft", function () {
     it("Should revert transferring NFT", async function () {
         const [, receiverSigner, toAddress] = await ethers.getSigners()
         const to = await toAddress.getAddress()
-        await soulboundNft.safeMint(receiver, "ipfs://testtokenuri")
+        await soulboundNft.safeMint(receiver)
         await expect(
             soulboundNft
                 .connect(receiverSigner)
